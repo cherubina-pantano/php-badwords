54104424f322b0b98c9f4061175b850a911cbec8
cherubina-pantano <cherubina.pantano@gmail.com>
2020-12-14 17:55:48 +0100

    testo censurato

--- index.php
+++ index.php
@@ -17,12 +17,22 @@
                 Con i Puffi puoi aiutarla se lo vuoi
                 Rispettiamo la natura ed allora il mondo guarirà';
       $len_text = strlen($text_song);
+      $badword = $_GET['badword'];
+      // REPLACE
+      $replaced = str_replace('Puffi', '***', $text_song);
     ?>
 
-    <h1>I Puffi sanno </h1>
+    <h1>Titolo canzone: I Puffi sanno </h1>
 
-    <p>  <?php echo $text_song; ?> </p>
-    <p> Il testo è lungo: <?php echo $len_text ?> caratteri.</p>
+    <p> Testo originale:  <?php echo $text_song; ?> </p>
+    <p> Il testo è lungo: <?php echo $len_text; ?> caratteri.</p>
+
+    <p> <a href="./index.php?badword=Puffi"> See badword: </a>
+       <?php echo $badword; ?>
+    </p>
+
+    <p>Testo censurato:  <?php echo $replaced; ?> </p>
+
 
   </body>
 </html>
