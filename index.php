387dbef3ee9b67c248ab9bc4107d59467f791720
cherubina-pantano <cherubina.pantano@gmail.com>
2020-12-14 15:36:50 +0100

    Add setup

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Badworks</title>
+  </head>
+  <body>
+    <h1>Ciao PHP</h1>
+
+    <?php
+      // codice php 
+    ?>
+  </body>
+</html>
